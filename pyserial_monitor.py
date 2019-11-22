c5d3d4e34f0f43d900fcc1c990e1c8953afd253a
Gabriel Ferraz <gferraz@gferraz>
2019-11-22 16:26:08 -0300

    Corrigido erros de compatibilidade no script Python e removido loops infinitos

--- pyserial_monitor.py
+++ pyserial_monitor.py
@@ -5,34 +5,60 @@ import serial
 import time
 import csv
 
-angle_data = []
-while True:
-    try:
-        serial_port = raw_input("Insira a porta serial/usb do Arduino: ")
-        serial_comm = serial.Serial(serial_port, 115200)
-        break
-    except:
-        print("Falha de comunicacao com a porta. Tente novamente.")
-        continue
+def run():
+    angle_data = []
+    tentativas = 0
+    num_max_tentativas = 10
 
-while True:
-    try:
-        float(serial_comm.readline())
-        break
-    except ValueError:
-        continue
-run_time = input("Insira o tempo de amostra em segundos: ")
-print("Iniciando amostragem.")
+    # Recebe a entrada e verifica se a porta de comunicacao serial e valida
+    while True:
+        try:
+            serial_port = input("Insira a porta serial/usb do Arduino: ")
+            serial_comm = serial.Serial(serial_port, 115200)
+            tentativas = 0
+            break
+        except:
+            print("Falha de comunicacao com a porta. Tente novamente.")
+            tentativas += 1
+            if tentativas > num_max_tentativas:
+                print("Programa falhou em estabelecer contato com a porta de comunicacao serial.")
+                return
+            continue
 
-start_time = time.time()
-while((time.time() - start_time) < run_time):
-    angle_data.append(float(serial_comm.readline()))
+    # Realiza chamadas para o Arduino ate conseguir receber o angulo do sensor
+    while True:
+        try:
+            float(serial_comm.readline())
+            break
+        except ValueError:
+            tentativas += 1
+            if tentativas > num_max_tentativas:
+                print("Erro ao receber dados do sensor.")
+                return
+            continue
 
-b, a = butter(3, 0.06)
-filtered_data = filtfilt(b, a, angle_data)
-plt.plot(filtered_data)
-plt.ylabel("Deslocamento Angular")
-plt.show()
+    while True:
+        try:
+            run_time = input("Insira o tempo de amostra em segundos: ")
+            break
+        except ValueError:
+            print("Valor invalido inserido.")
+            continue
 
-wtr = csv.writer(open('output.csv', 'w'), delimiter=',', lineterminator='\n')
-for x in filtered_data: wtr.writerow([x])
+    print("Iniciando amostragem.")
+
+    start_time = time.time()
+    while((time.time() - start_time) < run_time):
+        angle_data.append(float(serial_comm.readline()))
+
+    b, a = butter(3, 0.06)
+    filtered_data = filtfilt(b, a, angle_data)
+    plt.plot(filtered_data)
+    plt.ylabel("Deslocamento Angular")
+    plt.show()
+
+    wtr = csv.writer(open('output.csv', 'w'), delimiter=',', lineterminator='\n')
+    for x in filtered_data: wtr.writerow([x])
+
+if __name__ == "__main__":
+    run()
